7d6600ef9bbd7a08735778398987a19f0760fa74
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-10-23 21:58:16 +0900

    一旦コミット

---
+++ my-first-nuxt-app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- -->
+  </div>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    console.log(
+      JSON.stringify(await
+        this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, ' ')
+    )
+  }
+}
+</script>

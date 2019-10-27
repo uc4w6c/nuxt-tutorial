a9d084bdcb407f98234467b4c205e833e39f408f
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-10-27 15:27:17 +0900

    chapter4完了

---
+++ nuxt-blog-service/app/pages/posts/_id.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="container posts-page">
+        <div style="flex: 1">
+            <el-card v-if="post">
+                <div slot="header" class="clearfix">
+                    <h2>{{post.title}}</h2>
+                    <small>by {{post.user.id}}</small>
+                </div>
+                <p>{{post.body}}</p>
+                <p class="text-right">
+                    <el-button :disabled="!isLoggedIn" type="warning" v-if="isLiked"
+                                @click="unlike" round>
+                        <span class="el-icon-star-on" />
+                        <span>{{post.likes.length}}</span>
+                    </el-button>
+                    <el-button :disabled="!isLoggedIn" type="warning" v-else
+                                @click="like" round>
+                        <span class="el-icon-star-off" />
+                        <span>{{post.likes.length}}</span>
+                    </el-button>
+                </p>
+                <p class="text-right">
+                    {{post.created_at | time}}
+                </p>
+            </el-card>
+            <p>
+                <nuxt-link to="/posts">&lt; 投稿一覧へ戻る</nuxt-link>
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+import moment from '~/plugins/moment'
+import { mapGetters, mapActions } from 'vuex'
+import cloneDeep from 'lodash.clonedeep'
+
+export default {
+    async asyncData({ store, route , error }) {
+        const { id } = route.params
+        if (store.getters['posts/posts'].find(p => p.id === id)) {
+            return
+        }
+        try {
+            // SSRだとここに遷移し失敗する(おそらくstore側の問題)
+            await store.dispatch('posts/fetchPost', { id })
+            if (!(store.getters['posts/posts'].find(p => p.id === this.$route.params.id))) {
+                throw new Error('post not found')
+            }
+        } catch (e) {
+            error({ statusCode: 404 })
+        }
+    },
+    computed: {
+        post() {
+            return this.posts.find(p => p.id === this.$route.params.id)
+        },
+        isLiked() {
+            if (!this.user) return false
+            return this.posts.find(p => p.id === this.user.id)
+        },
+        ...mapGetters(['user', 'isLoggedIn']),
+        ...mapGetters('posts', ['posts'])
+    },
+    methods: {
+        like() {
+            if (!this.isLoggedIn) {
+                return
+            }
+            const likePayload = { user: this.user, post: this.post }
+            this.addLikeToPost(cloneDeep(likePayload))
+            this.addLikeLogToUser(cloneDeep(likePayload))
+        },
+        unlike() {
+            if (!this.isLoggedIn) {
+                return
+            }
+            // TODO: いいね取り消し処理を追加する
+        },
+        ...mapActions(['addLikeLogToUser']),
+        ...mapActions('posts', ['addLikeToPost'])
+    },
+    filters: {
+        time(val) {
+            return moment(val).format('YYYY/MM/DD HH:mm:ss')
+        }
+    }
+}
+</script>
+
+<style>
+.posts-page .el-table__row {
+    cursor: pointer;
+}
+</style>

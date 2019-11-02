227f638ebc4b4999bd3cd8168c586f4f2ee43e4e
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-11-02 23:01:33 +0900

    nuxtJSのSSRだけサンプルプログラム

---
+++ nuxt-vue-ssr/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>{{ title }} nuxt-app is running!</span>
+  </div>
+</template>
+
+<script>
+export default {
+  // CSR実行される
+  // data() {
+  // CSRは実行されない
+  asyncData() {
+    console.log('data Start')
+    return {
+      title: 'Nuxt SSR!'
+    }
+  }
+}
+</script>
+
